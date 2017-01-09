32e1bd57e77b53c70c04fc94cf59fcefd83c1b34
Tyler Youngs <tyler.youngs646@gmail.com>
2017-01-08 20:20:29 -0500

    aded files

---
+++ AutomatedRedditImageDownloaderClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace AutomatedRedditImageDownloaderClient
+{
+    public class Program
+    {
+        private static string _localDownloadLocation = @".\Downloads";
+        private static string _blacklistUrl = @"http://earth.tyleryoungs.com/Blacklist";
+        private static string _whiteListUrl = @"http://earth.tyleryoungs.com/Whitelist";
+        private static string _serverDownloadLocation = @"http://earth.tyleryoungs.com/Pictures/";
+
+        public static void Main(string[] args)
+        {
+            var imagesAlreadyDownloaded = GetImagesAlreadyDownloaded();
+            AddWhitelistedImages(imagesAlreadyDownloaded);
+            RemoveBlacklistedImages(imagesAlreadyDownloaded);
+        }
+
+        private static List<string> GetFileNamesFromSource(string url)
+        {
+            var client = new WebClient();
+            var whiteListHtml = client.DownloadString(url);
+            return whiteListHtml.Split(',').ToList();
+        }
+
+        private static List<string> GetImagesAlreadyDownloaded()
+        {
+            if (!Directory.Exists(_localDownloadLocation))
+            {
+                Directory.CreateDirectory(_localDownloadLocation);
+            }
+
+            return (new DirectoryInfo(_localDownloadLocation)).GetFiles("*.jpg").Select(x => x.Name).ToList();
+        }
+
+        private static void AddWhitelistedImages(List<string> imagesAlreadyDownloaded)
+        {
+            var whiteListNamesFromSource = GetFileNamesFromSource(_whiteListUrl);
+
+            foreach (var name in whiteListNamesFromSource)
+            {
+                if (!imagesAlreadyDownloaded.Contains(name) && !string.IsNullOrEmpty(name))
+                {
+                    var localPath = Path.Combine(_localDownloadLocation, name);
+                    var serverPath = Path.Combine(_serverDownloadLocation, name);
+
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile(new Uri(serverPath), localPath);
+                    }
+                }
+            }
+        }
+
+        private static void RemoveBlacklistedImages(List<string> imagesAlreadyDownloaded)
+        {
+            var blackListNamesFromSource = GetFileNamesFromSource(_blacklistUrl);
+
+            foreach (var name in blackListNamesFromSource)
+            {
+                if (imagesAlreadyDownloaded.Contains(name))
+                {
+                    var path = Path.Combine(_localDownloadLocation, name);
+                    File.Delete(path);
+                }
+            }
+        }
+    }
+}
